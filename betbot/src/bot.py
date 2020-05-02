64d0268e4fcf38b4d74e3ef03d5dacfafa0311f0
Nick Pyren <nick.pyren@gmail.com>
2020-05-02 15:56:21 -0400

    Initial commit

---
+++ betbot/src/bot.py
@@ -0,0 +1,25 @@
+import os
+
+import discord
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+assert TOKEN, "A token is required"
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print(f'{client.user} has connected to Discord!')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+client.run(TOKEN)
